90d3795abd8e3d03e09fd08a870068ee341baf1f
Tykyrah Strickland <tykyrah.strickland@bison.howard.edu>
2026-02-26 12:48:41 -0500

    Lab 2: VIBE refactor of NavLink component

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,22 +1,23 @@
-import { NavLink as RouterNavLink, NavLinkProps } from "react-router-dom";
-import { forwardRef } from "react";
+import { forwardRef, type ComponentPropsWithoutRef } from "react";
+import { NavLink as RouterNavLink } from "react-router-dom";
 import { cn } from "@/lib/utils";
 
-interface NavLinkCompatProps extends Omit<NavLinkProps, "className"> {
+type RouterNavLinkProps = ComponentPropsWithoutRef<typeof RouterNavLink>;
+
+interface NavLinkProps extends Omit<RouterNavLinkProps, "className"> {
   className?: string;
   activeClassName?: string;
   pendingClassName?: string;
 }
 
-const NavLink = forwardRef<HTMLAnchorElement, NavLinkCompatProps>(
-  ({ className, activeClassName, pendingClassName, to, ...props }, ref) => {
+const NavLink = forwardRef<HTMLAnchorElement, NavLinkProps>(
+  ({ className, activeClassName, pendingClassName, ...props }, ref) => {
     return (
       <RouterNavLink
-        ref={ref}
-        to={to}
         className={({ isActive, isPending }) =>
           cn(className, isActive && activeClassName, isPending && pendingClassName)
         }
+        ref={ref}
         {...props}
       />
     );
